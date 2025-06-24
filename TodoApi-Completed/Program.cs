8b3ed85e7709efa13c33fe6725aebfd076dab15f
FarhanAli06 <farhan.ali265@gmail.com>
2025-06-24 04:55:31 +0100

    added yml file for CI/CD pipeline configuration

--- TodoApi-Completed/Program.cs
+++ TodoApi-Completed/Program.cs
@@ -12,13 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+//builder.WebHost.UseUrls("http://localhost:8082;https://localhost:8081");
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();
 var db = scope.ServiceProvider.GetService<TodoContext>();
 DbInitializer.Initialize(db);
 
-DbInitializer.Initialize(db);
+//DbInitializer.Initialize(db);
+
+//Health Check
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -47,7 +53,14 @@ app.UseEndpoints(endpoints =>
 
 
 
-app.Run();
+try
+{
+    app.Run();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
 
 public partial class Program
 {
